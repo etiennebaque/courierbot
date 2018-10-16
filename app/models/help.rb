f6b9afb13484da24667bfbe942c8c469f5071551
Etienne Baqué <etiennebaque@gmail.com>
2018-10-15 20:08:41 -0400

    Rewrote help message

--- app/models/help.rb
+++ app/models/help.rb
@@ -1,12 +1,7 @@
 class Help < Message
 
   def response
-    "Courier bot - Help\n
-     ------------------\n
-     - To request a need, send:\n
-     need <description of your request>\n
-     - To report an event, send:\n
-     report <description of the event>"
+    "Courier bot - Help \n - To request a need, send: \n need <description of your request> \n - To report an event, send: \n report <description of the event>"
   end
 
 end
