f24139a59eb96e685c245ada1445069243451c33
Etienne Baqué <etiennebaque@gmail.com>
2018-10-21 10:31:37 -0400

    Renames login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admins, skip: [:sessions]
+  devise_scope :admin do
+    get 'login', to: 'devise/sessions#new', as: :new_admin_session
+    post 'login', to: 'devise/sessions#create', as: :admin_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_admin_session
+  end
 
-  devise_for :admins
   post 'sms/receive'
   resources :users
 
