b9d185d1bcd214b4c4f9919644656b2ab5921c1d
Etienne Baqué <etiennebaque@gmail.com>
2018-10-22 20:49:28 -0400

    Created user form on the home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,22 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def new
-  end
-
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "#{@user.name} (#{@user.phone_numer}) has been registered."
+    else
+      flash[:error] = "Please enter a name and a phone number"
+    end
+    redirect_to root_path
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :phone_number)
   end
 end
