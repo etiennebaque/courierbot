1efb5ab57ddfd33a9bcd41761fdd5f01cdc3dab1
Etienne Baqué <etiennebaque@gmail.com>
2018-10-22 21:29:38 -0400

    Added an about page

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
 
   root 'home#index'
   get 'home/index'
+  get 'home/about', as: :about
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
