853120949eb018f01bfd050ee0580633b4f357f9
Etienne Baqué <etiennebaque@gmail.com>
2018-10-22 21:11:29 -0400

    Implemented user deletion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "#{@user.name} (#{@user.phone_numer}) has been registered."
+      flash[:notice] = "#{@user.name} (#{@user.phone_number}) has been registered."
     else
       flash[:error] = "Please enter a name and a phone number"
     end
@@ -16,6 +16,13 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      flash[:notice] = "#{@user.name}'s profile has been deleted."
+    else
+      flash[:error] = "An error ocurred."
+    end
+    redirect_to users_path
   end
 
   private
