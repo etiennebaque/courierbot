1e740aeee537655d102a6e3f4cd11cb8f6c75abc
Etienne Baqué <mail@etiennebaque.com>
2019-03-17 13:32:40 -0400

    Added spec file for Help

--- app/models/help.rb
+++ app/models/help.rb
@@ -6,9 +6,11 @@ class Help < Message
     "Courier bot - Help \n - To request a need, send: \n need <description of your request> \n - To report an event, send: \n report <description of the event>"
   end
 
-
   def to_be_broadcasted?
     false
   end
 
+  def trigger_sms_text
+    TRIGGER_SMS_TEXT
+  end
 end
